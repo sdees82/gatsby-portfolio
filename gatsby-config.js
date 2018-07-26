6155f9c870b98f958ce95e4cf22e70b15a3ba566
sdees82 <seantedees1982@gmail.com>
2018-07-26 16:33:11 -0400

    readding favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,39 @@ module.exports = {
           `dosis`,
           `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ]
+      },
+      
+    }, {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.ico",
+  
+        // WebApp Manifest Configuration
+        appName: 'Gatsby site',
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+  
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
       }
     }
   ]
