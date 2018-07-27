85239ddf9e5ac05ca17948eff0ac55d460e190fc
sdees82 <seantedees1982@gmail.com>
2018-07-27 14:11:52 -0400

    fixing contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,46 +3,39 @@ import { CSSTransitionGroup } from "react-transition-group";
 import Arrow from "../images/blackArrow.png";
 import Link from "gatsby-link";
 import Navbar from "../components/navbar";
-import Video from "../components/video";
+import Video from '../components/video';
+
 
 const Contact = () => {
   return (
-    <div>
-      <Navbar />
-      <Video />
-      <div className="overlay" />
-      <header className="work">
-        <CSSTransitionGroup
-          transitionName="workTransition"
-          transitionAppear={true}
-          transitionAppearTimeout={500}
-          transitionEnter={false}
-          transitionLeave={false}
-        >
-          <form
-            className="form"
-            name="contactForm"
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            action="/thanks"
-          >
-            <input type="hidden" name="bot-field" />
-            <h1>CONTACT ME</h1>
-            <p type="Name:">
-              <input type="text" name="name" placeholder="" />
-            </p>
-            <p type="Email:">
-              <input type="email" name="email" placeholder="" />
-            </p>
-            <p type="Message:">
-              <textarea name="message" />
-            </p>
-            <button>Send Message</button>
-          </form>
-        </CSSTransitionGroup>
-      </header>
-      <Link to="/work" className="arrowBack">
+      <div>
+          <Navbar/>
+          <Video/>
+          <div className="overlay"/>
+    <header className="work">
+      <CSSTransitionGroup
+        transitionName="workTransition"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}
+      >
+        <form 
+        className="form"
+        name="contactForm" 
+        method="POST" 
+        data-netlify="true"
+        data-netlify-honeypot = "bot-field"
+        action="/thanks">
+  <h1>CONTACT ME</h1>
+  <p type="Name:"><input type="text" name="name" placeholder=""></input></p>
+  <p type="Email:"><input placeholder=""></input></p>
+  <p type="Message:"><textarea></textarea></p>
+  <button>Send Message</button>
+</form>
+      </CSSTransitionGroup>
+    </header>
+    <Link to="/work" className="arrowBack">
         <img src={Arrow} alt="arrow icon" />
       </Link>
       <Link to="/" className="arrow">
